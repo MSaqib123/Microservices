8f4495aea187a864225d114e99d5cad981d463e1
MBoota <m43577535@gmail.com>
2024-03-15 13:04:36 +0500

    Saqib

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -50,6 +50,9 @@ namespace Mango.Web.Controllers
             }
         }
 
+
+
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -97,12 +100,15 @@ namespace Mango.Web.Controllers
             return View(obj);
         }
 
+
+
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync();
             _tokenProvider.ClearToken();
             return RedirectToAction("Index","Home");
         }
+
 
         private async Task SignInUser(LoginResponseDto model)
         {
